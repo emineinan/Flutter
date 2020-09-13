7223c2ff2a0a38a99ee861a3cdc2b899a3769010
emineinan <emineinan@users.noreply.github.com>
2020-09-13 11:01:51 +0300

    dbHelper.dart is added.

--- sqflite_sample/lib/models/product.dart
+++ sqflite_sample/lib/models/product.dart
@@ -6,4 +6,21 @@ class Product {
 
   Product({this.name, this.description, this.unitPrice});
   Product.withId({this.id, this.name, this.description, this.unitPrice});
+
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    map["name"] = name;
+    map["description"] = description;
+    map["unitPrice"] = unitPrice;
+    if (id != null) {
+      map["id"] = id;
+    }
+  }
+
+  Product.fromObject(dynamic o) {
+    this.id = o["id"];
+    this.name = o["name"];
+    this.description = o["description"];
+    this.unitPrice = o["unitPrice"];
+  }
 }
