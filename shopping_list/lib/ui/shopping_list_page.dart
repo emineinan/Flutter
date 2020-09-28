08e2ffe1071e3e36e1729cc490f0330a27dd18e3
emineinan <emineinan@users.noreply.github.com>
2020-09-28 17:44:08 +0300

    project init.

---
+++ shopping_list/lib/ui/shopping_list_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ShoppingListPage extends StatefulWidget {
+  @override
+  _ShoppingListPageState createState() => _ShoppingListPageState();
+}
+
+class _ShoppingListPageState extends State<ShoppingListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping List"),
+      ),
+      body: Container(),
+    );
+  }
+}
