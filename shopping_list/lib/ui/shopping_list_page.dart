345411543541032c733b94bc17387b1d745944ff
emineinan <emineinan@users.noreply.github.com>
2020-09-29 11:49:49 +0300

    Dialog, BottomNavigationBar and data communication with Navigator is added.

--- shopping_list/lib/ui/shopping_list_page.dart
+++ shopping_list/lib/ui/shopping_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/ui/dialog/item_dialog.dart';
 
 class ShoppingListPage extends StatefulWidget {
   @override
@@ -6,13 +7,61 @@ class ShoppingListPage extends StatefulWidget {
 }
 
 class _ShoppingListPageState extends State<ShoppingListPage> {
+  int _selectedIndex = 0;
+  final PageController _pageController = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Shopping List"),
-      ),
-      body: Container(),
+        appBar: AppBar(
+          title: Text("Shopping List"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            String itemName = await showDialog(
+              context: context,
+              builder: (BuildContext context) => ItemDialog(),
+            );
+            print(itemName);
+          },
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: buildBottomNavigationBar(),
+        body: buildPageView());
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.list), title: Text("Shoppling List")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.history), title: Text("History")),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: (value) {
+        setState(() {
+          _selectedIndex = value;
+        });
+        _pageController.jumpToPage(value);
+      },
+    );
+  }
+
+  PageView buildPageView() {
+    return PageView(
+      controller: _pageController,
+      children: <Widget>[
+        Container(
+          color: Colors.purpleAccent,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.yellowAccent,
+        ),
+      ],
     );
   }
 }
