751e608fbdb2f71e19085b5ef0c4f68944b96270
emineinan <emineinan@users.noreply.github.com>
2020-11-08 09:34:01 +0300

    user model created.

--- coffee_app/lib/screens/authenticate/sign_in.dart
+++ coffee_app/lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_app/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -8,13 +9,23 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _authService = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: RaisedButton(child: Text("Login Anonymously"), onPressed: () {}),
+        child: RaisedButton(
+            child: Text("Login Anonymously"),
+            onPressed: () async {
+              dynamic result = await _authService.signInAnon();
+              if (result != null) {
+                return result.uid;
+              } else {
+                print("Login failed!");
+              }
+            }),
       ),
     );
   }
--- coffee_app/lib/services/auth.dart
+++ coffee_app/lib/services/auth.dart
@@ -1,13 +1,18 @@
+import 'package:coffee_app/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  User _comingFromFirebase(FirebaseUser fUser) {
+    return fUser != null ? User(userId: fUser.uid) : null;
+  }
+
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _comingFromFirebase(user);
     } catch (e) {
       print(e.toString());
       return null;
