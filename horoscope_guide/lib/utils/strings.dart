758eb4b5c30abb4b2cc2019842585d0fe9de2b39
emineinan <emineinan@users.noreply.github.com>
2020-11-15 22:07:15 +0300

    project init.

---
+++ horoscope_guide/lib/utils/strings.dart
@@ -0,0 +1,49 @@
+class Strings {
+  static const String APP_NAME = "HOROSCOPE GUİDE";
+
+  static const List<String> HOROSCOPE_NAMES = [
+    "Capricorn",
+    "Aquarius",
+    "Pisces",
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius"
+  ];
+
+  static const List<String> HOROSCOPE_DATES = [
+    "December 22 – January 19",
+    "January 20 – February 18",
+    "February 19 – March 20",
+    "March 21 – April 19",
+    "April 20 – May 20",
+    "May 21 – June 20",
+    "June 21 – July 22",
+    "July 23 – August 22",
+    "August 23 – September 22",
+    "September 23 – October 22",
+    "October 23 – November 21",
+    "November 22 – December 21",
+    "December 22 – January 19",
+  ];
+
+  static const List<String> HOROSCOPE_FEATURES = [
+    "Strengths: responsible, disciplined, self-control, good managers \nWeaknesses: know-it-all, unforgiving, condescending, expecting the worst",
+    "Strengths: Progressive, original, independent, humanitarian \nWeaknesses: Runs from emotional expression, temperamental, uncompromising, aloof",
+    "Strengths: Compassionate, artistic, intuitive, gentle, wise, musical \nWeaknesses: Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr",
+    "Strengths: courageous, determined, confident, enthusiastic, optimistic, honest, passionate \nWeaknesses: impatient, moody, short-tempered, impulsive, aggressive",
+    "Strengths: reliable, patient, practical, devoted, responsible, stable \nWeaknesses: stubborn, possessive, uncompromising",
+    "Strengths: gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas \nWeaknesses: nervous, inconsistent, indecisive",
+    "Strengths: tenacious, highly imaginative, loyal, emotional, sympathetic, persuasive \nWeaknesses: moody, pessimistic, suspicious, manipulative, insecure",
+    "Strengths: creative, passionate, generous, warm-hearted, cheerful, humorous \nWeaknesses: arrogant, stubborn, self-centered, lazy, inflexible",
+    "Strengths: loyal, analytical, kind, hardworking, practical \nWeaknesses: shyness, worry, overly critical of self and others, all work and no play",
+    "Strengths: cooperative, diplomatic, gracious, fair-minded, social \nWeaknesses: indecisive, avoids confrontations, will carry a grudge, self-pity",
+    "Strengths: resourceful, brave, passionate, stubborn, a true friend \nWeaknesses: distrusting, jealous, secretive, violent",
+    "Strengths: generous, idealistic, great sense of humor \nWeaknesses: promises more than can deliver, very impatient, will say anything no matter how undiplomatic",
+  ];
+}
